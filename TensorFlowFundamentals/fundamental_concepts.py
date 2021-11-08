f4b42585c93bf9e37b15b59a38120cfd6b8b7e00
Mihai Andreescu <mihaiandreescu2014@gmail.com>
2021-11-08 22:09:34 +0200

    2nd

--- TensorFlowFundamentals/fundamental_concepts.py
+++ TensorFlowFundamentals/fundamental_concepts.py
@@ -2,8 +2,6 @@ import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 import tensorflow as tf
 
-# print(tf.__version__)
-
 scalar = tf.constant(7)
 if __name__ == '__main__':
     print(scalar, "dimensions:",  scalar.ndim)
